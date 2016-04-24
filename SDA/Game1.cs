8de5457d7e64bdfb9db8fe2616c69c9282b1a5bd
ilw7159 <ilw7159@g.rit.edu>
2016-04-24 14:18:34 -0400

    Added basic menu functionality. Control with arrow keys and enter. Also has a GameOver screen that is implemented as a part of the MenuState, and it should loop back to the main menu (or exit), but that ahsnt been added yet

--- SDA/Game1.cs
+++ SDA/Game1.cs
@@ -9,7 +9,9 @@ namespace SDA
     /// </summary>
     public class Game1 : Game
     {
-        enum GameState { StartMenu, Instruction, LevelSelect, Game, GameOver } 
+        enum GameState { Menu, LevelSelect, Game, GameOver }
+        enum MenuState { Menu1, Menu2, Instruct1, Instruct2, Controls1, Controls2, GameOver1, GameOver2 }
+        MenuState menu;
 
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
@@ -18,6 +20,14 @@ namespace SDA
         Map gameMap;
         GameState currentGameState;
         Ghoul test;
+        Texture2D background1; //differnet menu images
+        Texture2D background2;
+        Texture2D background3;
+        Texture2D background4;
+        Texture2D background5;
+        Texture2D background6;
+        Texture2D background7;
+        Texture2D background8;
 
         //Temporary bool to test the enemy spawning, should change when map transitioning is in
         bool mapLoaded;
@@ -48,7 +58,8 @@ namespace SDA
 
             playerCharacter = new Player(new Vector2(64, 64), "Character/Player");
             gameMap = new Map(this.Content);
-            currentGameState = GameState.StartMenu;
+            currentGameState = GameState.Menu;
+            menu = MenuState.Menu1;
            // test = new Ghoul(new Vector2(128, 128), "Character/Ghoul");
             base.Initialize();
         }
@@ -67,6 +78,14 @@ namespace SDA
         //    test.LoadContent(this.Content);
             gameMap.LoadLevels();
             // TODO: use this.Content to load your game content here
+            background1 = Content.Load<Texture2D>("Menu1");
+            background2 = Content.Load<Texture2D>("Menu2");
+            background3 = Content.Load<Texture2D>("Instruct1");
+            background4 = Content.Load<Texture2D>("Instruct2");
+            background5 = Content.Load<Texture2D>("Controls1");
+            background6 = Content.Load<Texture2D>("Controls2");
+            background7 = Content.Load<Texture2D>("GameOver1");
+            background8 = Content.Load<Texture2D>("GameOver2");
         }
 
         /// <summary>
@@ -76,6 +95,164 @@ namespace SDA
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
+        }
+
+        //allows the menus to change between each other, allowing for
+        //proper transitions. Uses the MenuState for the changes
+        //called in the update method
+        public void ChangeMenu()
+        {
+            KeyboardState state = Keyboard.GetState();
+            switch (menu)
+            {
+                case MenuState.Menu1:
+                    if (state.IsKeyDown(Keys.Down))
+                    {
+                        menu = MenuState.Menu2;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        //start the game
+                        System.Threading.Thread.Sleep(500);
+                        currentGameState = GameState.Game;
+                        menu = MenuState.Menu1;
+                        gameMap.LoadFloor();
+                        break;
+                    }
+                    else { break; }
+                case MenuState.Menu2:
+                    if (state.IsKeyDown(Keys.Up))
+                    {
+                        menu = MenuState.Menu1;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        menu = MenuState.Instruct1;
+                        System.Threading.Thread.Sleep(500);
+                        break;
+                    }
+                    else { break; }
+                case MenuState.Instruct1:
+                    if (state.IsKeyDown(Keys.Right))
+                    {
+                        menu = MenuState.Instruct2;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        //start the game
+                        menu = MenuState.Menu1;
+                        System.Threading.Thread.Sleep(500);
+                        break;
+                    }
+                    else { break; }
+                case MenuState.Instruct2:
+                    if (state.IsKeyDown(Keys.Left))
+                    {
+                        menu = MenuState.Instruct1;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        menu = MenuState.Controls1;
+                        System.Threading.Thread.Sleep(500);
+                        break;
+                    }
+                    else { break; }
+                case MenuState.Controls1:
+                    if (state.IsKeyDown(Keys.Right))
+                    {
+                        menu = MenuState.Controls2;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        menu = MenuState.Menu1;
+                        System.Threading.Thread.Sleep(500);
+                        break;
+                    }
+                    else { break; }
+                case MenuState.Controls2:
+                    if (state.IsKeyDown(Keys.Left))
+                    {
+                        menu = MenuState.Controls1;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        System.Threading.Thread.Sleep(500);
+                        currentGameState = GameState.Game;
+                        menu = MenuState.Menu1;
+                        gameMap.LoadFloor();
+                        break;
+                    }
+                    else { break; }
+                case MenuState.GameOver1:
+                    if (state.IsKeyDown(Keys.Right))
+                    {
+                        menu = MenuState.GameOver2;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        System.Threading.Thread.Sleep(500);
+                        menu = MenuState.Menu1;
+                        break;
+                    }
+                    else { break; }
+                case MenuState.GameOver2:
+                    if (state.IsKeyDown(Keys.Left))
+                    {
+                        menu = MenuState.GameOver1;
+                        break;
+                    }
+                    else if (state.IsKeyDown(Keys.Enter))
+                    {
+                        System.Threading.Thread.Sleep(500);
+                        Exit();
+                        break;
+                    }
+                    else { break; }
+            }
+        }
+
+        //The method to draw whichever menu is currently active.
+        public void DrawMenu()
+        {
+            if (menu == MenuState.Menu1)
+            {
+                spriteBatch.Draw(background1, new Vector2(0.0f, 0.0f), Color.White);
+            }
+            else if (menu == MenuState.Menu2)
+            {
+                spriteBatch.Draw(background2, new Vector2(0.0f, 0.0f), Color.White);
+            }
+            else if (menu == MenuState.Instruct1)
+            {
+                spriteBatch.Draw(background3, new Vector2(0.0f, 0.0f), Color.White);
+            }
+            else if (menu == MenuState.Instruct2)
+            {
+                spriteBatch.Draw(background4, new Vector2(0.0f, 0.0f), Color.White);
+            }
+            else if (menu == MenuState.Controls1)
+            {
+                spriteBatch.Draw(background5, new Vector2(0.0f, 0.0f), Color.White);
+            }
+            else if (menu == MenuState.Controls2)
+            {
+                spriteBatch.Draw(background6, new Vector2(0.0f, 0.0f), Color.White);
+            }
+            else if (menu == MenuState.GameOver1)
+            {
+                spriteBatch.Draw(background7, new Vector2(0.0f, 0.0f), Color.White);
+            }
+            else if (menu == MenuState.GameOver2)
+            {
+                spriteBatch.Draw(background8, new Vector2(0.0f, 0.0f), Color.White);
+            }
         }
 
         /// <summary>
@@ -89,13 +266,9 @@ namespace SDA
                 Exit();
 
             // TODO: Add your update logic here
-            if (currentGameState == GameState.StartMenu)
+            if (currentGameState == GameState.Menu)
             {
-                    if (Keyboard.GetState().IsKeyDown(Keys.Enter))
-                    {
-                        currentGameState = GameState.Game;
-                        gameMap.LoadFloor();
-                    }
+                ChangeMenu();
             }
             else if (currentGameState == GameState.Game)
             {
@@ -124,7 +297,7 @@ namespace SDA
 
                 }
             }
-            else if (currentGameState == GameState.GameOver)
+            /*else if (currentGameState == GameState.GameOver)
             {
 
             }
@@ -135,7 +308,7 @@ namespace SDA
             else if (currentGameState == GameState.LevelSelect)
             {
                 
-            }
+            }*/
 
             base.Update(gameTime);
         }
@@ -152,6 +325,11 @@ namespace SDA
             // TODO: Add your drawing code here
             spriteBatch.Begin();
             
+            if (currentGameState == GameState.Menu)
+            {
+                DrawMenu();
+            }
+
             if (currentGameState == GameState.Game)
             {
                 gameMap.Draw(spriteBatch);
